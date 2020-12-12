a4a8b8306a23d63f8721ed397d68edefeea26d55
TroyPerez <eduardo.pereztmw@hotmail.com>
2020-12-12 02:16:07 -0600

    prueba de servidor con express js

--- tutorexpress/resources/js/app.js
+++ tutorexpress/resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+//require('./bootstrap');
+const express = require('express');
+const app = express();
+
+//crear servicod http con express
+const http = require('http').Server(app);
+//paara generar comunicacion, usar socket io
+const io = require('socket.io')(http);
+
+//routes
+app.use(require('./clase.routes'));
+//cargar estaticos html
+app.use(express.static("../views/"));
+
+module.exports = http;
---
+++ tutorexpress/resources/js/index.js
@@ -0,0 +1,5 @@
+const http = require('./app');
+
+http.listen(3000, () => {
+	console.log('servidor en funcionamiento');
+});
