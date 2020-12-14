392abf8314207d7c337ec00d4d9682db9c9dd8b9
TroyPerez <eduardo.pereztmw@hotmail.com>
2020-12-13 19:26:38 -0600

    agregada la funcion del videochat

--- tutorexpress/routes/web.php
+++ tutorexpress/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Pusher\Pusher;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//Route::group([‘middleware’ => ‘auth’], function(){  
+	//Route::get(‘video_chat’, ‘VideoChatController@index’);  
+	//Route::post(‘auth/video_chat’, ‘VideoChatController@auth’);
+//});
